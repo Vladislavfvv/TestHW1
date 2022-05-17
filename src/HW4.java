26f5f634ebc27512f10f5e3ae3f67fc6d38b415d
Dimaf <vladfilimontsev@yandex.ru>
2022-05-17 11:39:44 +0300

    Add_new in HW4 until 7 task

--- src/HW4.java
+++ src/HW4.java
@@ -16,6 +16,27 @@ public class HW4 {
                 "Для генерации случайного сичла используйте метод Math.random()." +
                 "Пусть  будет возможность создавать массив произвольног размера" +
                 "и вводится он с консоли.");
+        System.out.println("3 - Создайте 2 массива. Выведите массивы на консоль." +
+                "Посчитайте среднее арифметическое элементов каждого массива и сообщите, " +
+                "для какого из массивов это значение оказалось больше (либо сообщите, " +
+                "что их средние арифметические равны.)");
+
+        //Additional tasks
+
+        System.out.println("4 - Создайте массив из n случайных чисели выведите его на экран." +
+                "Размер массива пусть задается из консоли и размер массива может быть больше 5 " +
+                "и меньше или равно 10. " +
+                "Если n не удовлетворяет условию - выведите сообщение об этом." +
+                "Если пользователь ввёл не подходящее число, то программа должна попросить пользователя повторить ввод." +
+                "Создайте 2 массив только из четных элементов первого массива, если они там есть, " +
+                "и вывести его на экран.");
+        System.out.println("5 - Создайте массив и заполните его." +
+                "Выведите массив на экран в строку." +
+                "Замените каждый нечетный элемент с нечетным индексом на ноль." +
+                "Снова выведите массив на экран на отдельной строке.");
+        System.out.println("6 - Создайте массив строк. Заполните его произвольными именами людей." +
+                "Отсортируйте массив. Результат выведите в консоль.");
+        System.out.println("7 - Сортировка пузырьком");
 
         Scanner scanner = new Scanner(System.in);
         int task = scanner.nextInt();
@@ -83,28 +104,228 @@ public class HW4 {
             }
 
             //Task2\\\\\\\\\\\\\\\\\\
-                case 2: {
-                    System.out.println("Введите размер массива");
-                    int size = scanner.nextInt();
-                    System.out.println("Введите диапазон значений массива до:");
-                    int r = scanner.nextInt();
-                    int[] tt = fillArr(size, r);
-                    System.out.println(Arrays.toString(tt));
+            case 2: {
+                System.out.println("Введите размер массива");
+                int size = scanner.nextInt();
+                System.out.println("Введите диапазон значений массива до:");
+                int range = scanner.nextInt();
+                int[] tt = fillArr(size, range);
+                System.out.println(Arrays.toString(tt));
+                minNumber(tt, size);
+                maxNumber(tt, size);
+                middleNumber(tt, size);
+
+            }
+
+            //Task3\\\\\\\\\\\\\\\\\\
+            case 3: {
+                while(true) {
+                    System.out.println("Введите размер 1 массива");
+                    int size1 = scanner.nextInt();
+                    System.out.println("Введите диапазон значений  1 массива до:");
+                    int range1 = scanner.nextInt();
+                    int[] tt = fillArr(size1, range1);
+                    System.out.println("Первый массив: " + Arrays.toString(tt));
+
+                    System.out.println("Введите размер 2 массива");
+                    int size2 = scanner.nextInt();
+                    System.out.println("Введите диапазон значений  1 массива до:");
+                    int range2 = scanner.nextInt();
+                    int[] tt2 = fillArr(size2, range2);
+                    System.out.println("Первый массив: " + Arrays.toString(tt2));
+
+                    int mid1 = average(tt, size1);
+                    int mid2 = average(tt2, size2);
+
+                    System.out.println("Среднее арифметическое первого массива: " + mid1);
+                    System.out.println("Среднее арифметическое второго массива: " + mid2);
+                    System.out.println();
+                    if (mid1 > mid2)
+                        System.out.println("Среднее арифметическое первого массива больше среднего арифметического второго");
+                    else if (mid1 < mid2)
+                        System.out.println("Среднее арифметическое второго массива больше среднего арифметического первого");
+                    else System.out.println("Средние арифметические двух массивов равны.");
+                    System.out.println("Would you like to repeat again? y/n");
+                    char c = (char) System.in.read();
+                    if (c == 'n') break;;
                 }
+            }
+
+            case 4: {
+                System.out.println("Write the size of the first array:");
+                int size1 = scanner.nextInt();
+
+                while ((size1 >= 10) || (size1 < 5)){
+                    System.out.println("The array size does not match. Enter a size from 5 to 10. Repeat enter.");
+                    System.out.println("Write the size of the first array:");
+                    size1 = scanner.nextInt();
+                };
+
+
+                int[] array = createRandomArray(size1);
+                System.out.println("Your first massive:");
+                System.out.println(Arrays.toString(array));
+
+                int[] array2 = createEvenArray(array);
+                System.out.println("Your modified even array:");
+                soutArray(array2, array.length);
+            }
+
+            case 5: {
+                int n = scanner.nextInt();
+                int[] arr5 = new int[n];
+                Random random = new Random();
+                for (int i = 0; i < n; i++) {
+                    arr5[i]  = random.nextInt(10);
+                }
+                System.out.println("Your massive:");
+                System.out.println(Arrays.toString(arr5));
+                for (int i = 0; i < arr5.length; i++) {
+                    if(arr5[i]%2 != 0){
+                        arr5[i] = 0;
+                    }
+                }
+                System.out.println(Arrays.toString(arr5));
+            }
+
+            case 6:{
+                String[] names = {"Sasha", "Tanya", "Dima", "Anton"};
+                System.out.println(Arrays.toString(names));
+                Arrays.sort(names);
+                System.out.println(Arrays.toString(names));
+            }
+
+            case 7:{
+
+                int[] arr7 = {7, 5, 1, 3, 8, 2};
+                System.out.println(Arrays.toString(arr7));
+                //Сортировка пузырьком
+                int temp = 0;
+                for (int i = 0; i < arr7.length; i++) {
+                    //temp = arr7[i];
+                    //int k = 0;
+                    for (int j = 0; j < arr7.length-j; j++) {
+                        if (arr7[j] > arr7[j + 1]){
+                            temp = arr7[j + 1];
+                            arr7[j + 1] = arr7[j];
+                            arr7[j] = temp;
+                        }
+                    }
+
+                }
+                System.out.println(Arrays.toString(arr7));
+
+            }
         }
 
     }
 
-public static int[] fillArr(int a, int b) {
+
+    public static int[] createEvenArray(int[] arr){
+        int[] b = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]%2 == 0)
+                b[i] = arr[i];
+        }
+        //сдвигаем влево элементы массива
+        int[] k = new int[b.length];
+        for (int i = 0, j = 0; i < b.length; i++) {
+            if(b[i] != 0)
+            k[j++] = b[i];
+        }
+
+        /*System.out.println("Array k:");
+        System.out.println(Arrays.toString(k));
+
+        int temp = 0;
+        for (int i = 0; i < k.length; i++) {
+            if (k[i] == 0)
+                temp = i;
+        }
+        System.out.println("temp:" + temp);
+        int[] k2 = new int[temp];
+        for (int i = 0; i < k2.length; i++) {
+            k2[i] = k[i];
+        }
+
+
+        System.out.println("Array k2:");
+        System.out.println(Arrays.toString(k2));
+*/
+
+
+        return b;
+    }
+
+    public static int[] createRandomArray (int n){
+        Random random = new Random();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = random.nextInt(10);
+        }
+        return arr;
+    }
+
+    public static void minNumber(int[] arr, int a) {
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (min > arr[i]) {
+                min = arr[i];
+            }
+        }
+        System.out.println("Минимальное значение в данном массиве: " + min);
+    }
+
+    public static void maxNumber(int[] arr, int a) {
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+        }
+        System.out.println("Максимальное значение в данном массиве: " + max);
+
+
+    }
+
+
+    public static int average(int[] arr, int b){
+        int sum = 0;
+        int aver = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        aver = Math.round(sum/b);
+
+        return aver;
+    }
+
+
+
+    public static void middleNumber(int[] arr, int a) {
+        int count = 0;
+        int sum = 0;
+        for (int i = 0; i < arr[i]; i++) {
+            sum += arr[i];
+            count++;
+        }
+        int middle = sum / count;
+        System.out.println("Среднее арифметическое в данном массиве: " + middle);
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+
+    public static int[] fillArr(int a, int b) {
         int[] arr = new int[a];
         for (int i = 0; i < arr.length; i++) {
-            Random random = new Random(b);
-            arr[i] = random.nextInt();
-    }
+            Random random = new Random();
+            arr[i] = random.nextInt(b);
+        }
         return arr;
-}
+    }
 
-    public static int[] soutArray(int arr[], int n){
+    public static int[] soutArray(int arr[], int n) {
         for (int i = 0; i < n; i++) {
             if (arr[i] != 0)
                 System.out.print(arr[i] + ", ");
@@ -119,7 +340,7 @@ public static int[] fillArr(int a, int b) {
                 System.arraycopy(m2, i + 1, newarr, i, new_size);
                 System.out.println(Arrays.toString(newarr));
         }*/
-    }
+}
 
 
 
